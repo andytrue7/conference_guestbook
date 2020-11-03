81c790215e6bc4a7e504ea39f7aa39e91487c3c6
Andrey <univer1857@gmail.com>
2020-11-03 13:41:22 +0200

    add admin panel

---
+++ src/Controller/Admin/CommentCrudController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CommentCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Comment::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('author'),
+            TextField::new('text'),
+            EmailField::new('email'),
+            DateField::new('createdAt'),
+            ImageField::new('photoFilename', 'Photo')->setBasePath('/uploads/photos'),
+            AssociationField::new('conference')
+        ];
+    }
+
+}
